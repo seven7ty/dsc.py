be07bfcda0915046398b6f1c9ef091e41cd1fac0
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-11 01:48:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("./README.md", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="dsc.py",
+    version="0.0.1",
+    author="itsmewulf",
+    author_email="wulf.developer@gmail.com",
+    description="A simple and easy to use, fully asynchronous wrapper for the dsc.gg API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itsmewulf/dsc.py",
+    keywords="api-wrapper, http, request, api, wrapper, discord",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
