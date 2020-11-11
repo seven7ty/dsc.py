a20b65b1b9acf1c8c1b6efa9c0b95cc9e03ec07d
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-11 02:56:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as file:
 
 setuptools.setup(
     name="dsc.py",
-    version="0.0.1",
+    version="0.1.0",
     author="itsmewulf",
     author_email="wulf.developer@gmail.com",
     description="A simple and easy to use, fully asynchronous wrapper for the dsc.gg API.",
