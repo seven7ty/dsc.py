728fac0b2f120502876c511620cc15096d791cf9
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-11 14:47:08 +0100

    Update user.py

--- dsc/models/user.py
+++ dsc/models/user.py
@@ -8,3 +8,10 @@ class User:
         self.premium: bool = str_bool(data.get("premium"))
         self.blacklisted: bool = str_bool(data.get("blacklisted"))
         self.staff: Any = null(data.get("staff"))
+        self.raw: dict = data
+
+    def __int__(self) -> int:
+        return self.links
+
+    def __bool__(self) -> bool:
+        return self.premium
