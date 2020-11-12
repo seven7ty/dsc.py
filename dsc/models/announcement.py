924860b4f8a7c226b8d1dd4d1182c137afe5bd96
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-12 22:44:13 +0100

    0.3.0

--- dsc/models/announcement.py
+++ dsc/models/announcement.py
@@ -1,3 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+MIT License
+
+Copyright (c) 2020 Paul Przybyszewski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from typing import Union
 
 
@@ -6,7 +32,7 @@ class Announcement:
         self.author: Union[str, None] = data.get("author", None)
         self.recipients: Union[str, None] = data.get("for", None)
         self.message: Union[str, None] = data.get("message", None)
-        self.character: Union[str, None] = data.get("type", None)
+        self.type: Union[str, None] = data.get("type", None)
         self.raw: dict = data
 
     def __str__(self) -> str:
