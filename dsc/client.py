5b1d97b188a274d603bb30d6a2987e0613f389ea
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-12 22:41:13 +0100

    0.3.0

--- dsc/client.py
+++ dsc/client.py
@@ -24,67 +24,130 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientTimeout
 from .models.link import Link
 from .models.user import User
 from .models.announcement import Announcement
 from .exceptions import (
-    NoToken, BadToken,
+    NoToken, Unauthorized,
     Forbidden, BearerNoToken,
-    BadLinkType
+    BadLinkType, InternalServerError,
+    BadRequest, ServiceUnavailable
 )
-from typing import Union, List
-from asyncio import sleep
+from typing import (Union, List,
+                    NoReturn, Any)
+from asyncio import (sleep, AbstractEventLoop,
+                     get_event_loop)
 
-
-async def _validate_link_type(link_type: str) -> bool:
-    valid: list = ["server", "bot", "template"]
-    return link_type.lower() in valid
+correlation: dict = {
+    400: BadRequest,
+    401: Unauthorized,
+    403: Forbidden,
+    500: InternalServerError,
+    503: ServiceUnavailable,
+    BadRequest: "Something went wrong",
+    Unauthorized: "The Discord OAuth Token you provided was invalid",
+    Forbidden: "You cannot perform this action",
+    InternalServerError: "The API returned a 500 status code",
+    ServiceUnavailable: "The dsc.gg API is currently unavailable"
+}
 
 
 class Client:
-    def __init__(self, token: str = None, bearer: bool = False):
-        self._token: str = token.strip()
+    def __init__(self, token: str = None, bearer: bool = False, verbose: bool = False, connection_timeout: int = 15):
+        if token is not None:
+            self._token: str = token.strip()
+        else:
+            self._token = None
+
         self._bearer: bool = bearer
+
         if self._bearer and self._token is None:
             raise BearerNoToken("If bearer is True, a token must be passed into the constructor")
-        if self._bearer:
-            self._token = "Bearer " + self._token
-        self._ses: ClientSession = ClientSession()
-        self._transfer_cache: list = []
 
-    async def _cache_transfer(self, link: Union[str, int]) -> None:
-        self._transfer_cache.append(link)
-        await sleep(60)
-        self._transfer_cache.remove(link)
+        if self._bearer and self._token[:7] != "Bearer ":
+            self._token: str = "Bearer " + self._token
+
+        self.connection_timeout: int = round(connection_timeout)
+        self._loop: AbstractEventLoop = get_event_loop()
+        self._ses: ClientSession = ClientSession(
+            loop=self._loop,
+            timeout=ClientTimeout(total=self.connection_timeout))
+        self.transfer_is_ratelimited: bool = False
+        self._verbose: bool = verbose
+
+    @staticmethod
+    def _validate_link_type(link_type: str) -> bool:
+        valid: list = ["server", "bot", "template"]
+        return link_type.lower() in valid
+
+    @staticmethod
+    def _error_from_status(code: int) -> Any:
+        return correlation.get(code, None)
+
+    async def _rate_limit_transfer(self) -> None:
+        self.transfer_is_ratelimited = True
+        await sleep(300)
+        self.transfer_is_ratelimited = False
 
     async def get_link(self, link: Union[str, int]) -> Union[Link, None]:
         res = await self._ses.get(f"https://dsc.gg/api/link/{link}")
+
         if res.status == 200:
             return Link(dict(await res.json()))
+
+        self._v(f"Couldn't fetch link '{link}'")
         return None
 
-    async def get_links(self, user_id: Union[int, str]) -> Union[List[Link], None]:
+    async def get_links(self, user_id: Union[int, str]) -> Union[List[Link], List]:
         res = await self._ses.get(f"https://dsc.gg/api/links/{user_id}")
+
         if res.status == 200:
             return [Link(dict(link)) for link in list(await res.json())]
-        return None
+
+        self._v(f"Couldn't fetch links for user ID '{user_id}', returning an empty list")
+        return []
+
+    async def top_links(self) -> Union[List[Link], List]:
+        res = await self._ses.get("https://dsc.gg/api/top-links")
+
+        if res.status == 200:
+            return [Link(dict(link)) for link in list(await res.json())]
+
+        self._v("Couldn't fetch top links, returning an empty list")
+        return []
+
+    async def fetch_links(self, page: int) -> Union[List[Link], List]:
+        res = await self._ses.get(f"https://dsc.gg/api/all-links?page={page if page > 0 else 1}")
+
+        if res.status == 200:
+            return [Link(dict(link)) for link in list(await res.json())]
+
+        self._v("Couldn't fetch links, returning an empty list")
+        return []
 
     async def get_user(self, user_id: Union[int, str]) -> Union[User, None]:
         res = await self._ses.get(f"https://dsc.gg/api/info/{user_id}")
+
         if res.status == 200:
             return User(dict(await res.json()))
+
+        self._v(f"Couldn't fetch user with the ID '{user_id}'")
         return None
 
-    async def get_announcements(self, user_id: Union[int, str]) -> list:
+    async def get_announcements(self, user_id: Union[int, str]) -> Union[List[Announcement], List]:
         res = await self._ses.get(f"https://dsc.gg/api/announcements/{user_id}")
-        return list([Announcement(dict(a)) for a in list(await res.json())])
 
-    async def create_link(self, link: Union[str, int], redirect: str, link_type: str) -> None:
-        if self._token is None:
-            raise NoToken("You need to pass in a Discord OAuth2 Token into the object constructor to use this function")
+        if res.status == 200:
+            return list([Announcement(dict(a)) for a in list(await res.json())])
 
-        if not await _validate_link_type(link_type):
+        self._v(f"Couldn't fetch announcement for user ID '{user_id}'")
+        return []
+
+    async def create_link(self, link: Union[str, int], redirect: str, link_type: str) -> NoReturn:
+        self._has_token()
+
+        if not self._validate_link_type(link_type):
             raise BadLinkType("link_type must be either 'bot', 'server' or 'template'")
 
         res = await self._ses.post("https://dsc.gg/api/create",
@@ -92,16 +155,15 @@ class Client:
                                    json={"link": link, "redirect": redirect, "type": link_type.lower(),
                                          "token": self._token})
         if res.status != 200:
-            if res.status == 401:
-                raise BadToken("Invalid OAuth2 token provided")
-            elif res.status == 403:
-                raise Forbidden("You cannot perform this action. (create_link)")
+            if (err := self._error_from_status(res.status)) is not None:
+                raise err(correlation.get(err))
 
-    async def update_link(self, link: Union[str, int], redirect: str, link_type: str) -> None:
-        if self._token is None:
-            raise NoToken("You need to pass in a Discord OAuth2 Token into the object constructor to use this function")
+        self._v(f"Link '{link}' created.")
 
-        if not await _validate_link_type(link_type):
+    async def update_link(self, link: Union[str, int], redirect: str, link_type: str) -> NoReturn:
+        self._has_token()
+
+        if not self._validate_link_type(link_type):
             raise BadLinkType("link_type must be either 'bot', 'server' or 'template'")
 
         res = await self._ses.post("https://dsc.gg/api/update",
@@ -110,38 +172,46 @@ class Client:
                                          "token": self._token})
 
         if res.status != 200:
-            if res.status == 401:
-                raise BadToken("Invalid OAuth2 token provided")
-            elif res.status == 403:
-                raise Forbidden("You cannot perform this action. (update_link)")
+            if (err := self._error_from_status(res.status)) is not None:
+                raise err(correlation.get(err))
 
-    async def delete_link(self, link: Union[str, int]) -> None:
-        if self._token is None:
-            raise NoToken("You need to pass in a Discord OAuth2 Token into the object constructor to use this function")
+        self._v(f"Link '{link}' updated.")
+
+    async def delete_link(self, link: Union[str, int]) -> NoReturn:
+        self._has_token()
 
         res = await self._ses.post("https://dsc.gg/api/delete",
                                    headers={"contentType": "application/json"},
                                    json={"link": link, "token": self._token})
 
         if res.status != 200:
-            if res.status == 401:
-                raise BadToken("Invalid OAuth2 token provided")
-            elif res.status == 403:
-                raise Forbidden("You cannot perform this action. (delete_link)")
+            if (err := self._error_from_status(res.status)) is not None:
+                raise err(correlation.get(err))
 
-    async def transfer_link(self, link: Union[str, int], user_id: Union[str, int], comments: str = "None") -> None:
-        if link in self._transfer_cache:
+        self._v(f"Link '{link}' deleted.")
+
+    async def transfer_link(self, link: Union[str, int], user_id: Union[str, int], comments: str = "None") -> NoReturn:
+        if self.transfer_is_ratelimited:
+            self._v(f"Cannot transfer link '{link}' to user ID '{user_id}' - ratelimited.")
             return
-        if self._token is None:
-            raise NoToken("You need to pass in a Discord OAuth2 Token into the object constructor to use this function")
+
+        self._has_token()
 
         res = await self._ses.post("https://dsc.gg/api/transfer",
                                    json={"link": link, "comments": comments, "transfer": str(user_id),
-                                         "token": self._token})
+                                         "token": self._token}, headers={"contentType": "application/json"})
 
         if res.status != 200:
-            if res.status == 401:
-                raise BadToken("Invalid OAuth2 token provided")
-            elif res.status == 403:
-                raise Forbidden("You cannot perform this action. (transfer_link)")
-        await self._cache_transfer(link=link)
+            if (err := self._error_from_status(res.status)) is not None:
+                raise err(correlation.get(err))
+        else:
+            self._v(f"Successful transfer to user ID '{user_id}' - 5 minute ratelimit")
+            self._loop.create_task(self._rate_limit_transfer())
+
+    def _has_token(self) -> NoReturn:
+        if self._token is None:
+            raise NoToken("You need to pass in a Discord OAuth2 Token into the object constructor to use this function")
+
+    def _v(self, msg: str) -> NoReturn:
+        if self._verbose:
+            print(msg)
