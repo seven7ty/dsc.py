33a89f9b0b3c72b71dfcbfbfc72b74fdfd30dc87
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-12 22:43:07 +0100

    0.3.0

--- dsc/utils.py
+++ dsc/utils.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from typing import Union
 
 
-def str_bool(str_bool: str) -> bool:
-    return True if str(str_bool).lower() == "true" else False
+def str_bool(val: str) -> bool:
+    return True if str(val).lower() == "true" else False
 
 
 def null(val: str) -> Union[str, None]:
     return None if str(val).lower() == 'null' else val
+
+
+def int_or_none(val: Union[str, None]) -> Union[int, None]:
+    return int(val) if val is not None else val
